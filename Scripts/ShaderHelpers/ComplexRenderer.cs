f80c3ed4025011076a8f378e75e26877720e1e19
jdugan0 <justin.d.dugan@gmail.com>
2025-05-18 14:08:36 -0400

    viewbase

--- Scripts/ShaderHelpers/ComplexRenderer.cs
+++ Scripts/ShaderHelpers/ComplexRenderer.cs
@@ -1,82 +1,16 @@
 using Godot;
 using System;
 
-public partial class ComplexRenderer : Sprite2D
+public partial class ComplexRenderer : ViewBase
 {
-    int pixelHeight;
-    int pixelWidth;
-    public ShaderMaterial material;
-    ImageTexture texture = new ImageTexture();
-    [Export] public TextEdit textEdit;
-    public float zoom = 0.1f;
-    [Export] public float speed;
-    public Vector2 offset = new Vector2(0, 0);
+    [Export] public TextEdit TextEdit;
 
-    [Export] public PauseMenu pause;
-    public override void _Ready()
+    public override void HandleInput(double delta)
     {
-        material = (ShaderMaterial)Material;
-        pixelHeight = GetViewport().GetWindow().Size.Y;
-        pixelWidth = GetViewport().GetWindow().Size.X;
-        Image image = Image.CreateEmpty(pixelWidth, pixelHeight, false, Image.Format.Rgba8);
-        texture.SetImage(image);
-        Texture = texture;
-        SendData(material);
-    }
-
-    public override void _Process(double delta)
-    {
-        material = (ShaderMaterial)Material;
-        Vector2 velocity = new Vector2();
-        bool doStuff = true;
-        if (textEdit != null)
+        if (TextEdit.HasFocus())
         {
-            if (textEdit.HasFocus())
-            {
-                doStuff = false;
-            }
+            return;
         }
-        if (doStuff && !pause.paused)
-        {
-            if (Input.IsActionPressed("UP"))
-            {
-                velocity += Vector2.Up;
-            }
-            if (Input.IsActionPressed("DOWN"))
-            {
-                velocity += Vector2.Down;
-            }
-            if (Input.IsActionPressed("LEFT"))
-            {
-                velocity += Vector2.Left;
-            }
-            if (Input.IsActionPressed("RIGHT"))
-            {
-                velocity += Vector2.Right;
-            }
-            if (Input.IsActionPressed("ZoomIn"))
-            {
-                zoom += (float)delta * 1f * zoom;
-            }
-            if (Input.IsActionPressed("ZoomOut"))
-            {
-                zoom -= (float)delta * 1f * zoom;
-            }
-            if (Input.IsActionJustPressed("Home"))
-            {
-                offset = new Vector2(0, 0);
-                zoom = 0.100f;
-            }
-        }
-        zoom = Mathf.Clamp(zoom, (float)1e-32, 999999);
-        velocity = velocity.Normalized() * (float)delta / zoom * speed;
-        offset += velocity;
-        SendData(material);
-    }
-
-    public void SendData(ShaderMaterial m)
-    {
-        m.SetShaderParameter("offset", offset);
-        m.SetShaderParameter("zoomFactor", zoom);
+        base.HandleInput(delta);
     }
 }
---
+++ Scripts/ShaderHelpers/ViewBase.cs
@@ -0,0 +1,63 @@
+// res://Scripts/ComplexViewBase.cs
+using Godot;
+using System;
+
+public partial class ViewBase : Sprite2D
+{
+    [Export] public float speed = 1f;
+    protected Vector2 offset = Vector2.Zero;
+    protected float zoom = 0.1f;
+
+    [Export] public PauseMenu pauseMenu;
+
+    int _w, _h;
+    ImageTexture _tex = new ImageTexture();
+    ShaderMaterial _mat;
+
+    public override void _Ready()
+    {
+        _w = GetViewport().GetWindow().Size.X;
+        _h = GetViewport().GetWindow().Size.Y;
+
+        // one-pixel-per-fragment backing texture
+        var img = Image.CreateEmpty(_w, _h, false, Image.Format.Rgba8);
+        _tex.SetImage(img);
+        Texture = _tex;
+
+        _mat = (ShaderMaterial)Material;
+        PushUniforms();            // first push
+    }
+
+    public override void _Process(double delta)
+    {
+        _mat = (ShaderMaterial)Material;
+        HandleInput(delta);
+        PushUniforms();
+    }
+    public virtual void HandleInput(double delta)
+    {
+        if (pauseMenu.paused)
+        {
+            return;
+        }
+        Vector2 v = Vector2.Zero;
+        if (Input.IsActionPressed("UP")) v += Vector2.Up;
+        if (Input.IsActionPressed("DOWN")) v += Vector2.Down;
+        if (Input.IsActionPressed("LEFT")) v += Vector2.Left;
+        if (Input.IsActionPressed("RIGHT")) v += Vector2.Right;
+
+        if (Input.IsActionPressed("ZoomIn")) zoom += (float)(delta) * zoom;
+        if (Input.IsActionPressed("ZoomOut")) zoom -= (float)(delta) * zoom;
+        if (Input.IsActionJustPressed("Home")) { offset = Vector2.Zero; zoom = 0.1f; }
+
+        zoom = Mathf.Clamp(zoom, 1e-32f, 1e9f);
+        offset += v.Normalized() * (float)delta / zoom * speed;
+    }
+
+    public virtual void PushUniforms()
+    {
+        if (_mat == null) return;
+        _mat.SetShaderParameter("offset", offset);
+        _mat.SetShaderParameter("zoomFactor", zoom);
+    }
+}
