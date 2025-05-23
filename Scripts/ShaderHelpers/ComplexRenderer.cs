1e9c84354bb7b08691c0bb351c461750afc53f13
jdugan0 <justin.d.dugan@gmail.com>
2025-05-23 11:33:57 -0400

    complex plotting

--- Scripts/ShaderHelpers/ComplexRenderer.cs
+++ Scripts/ShaderHelpers/ComplexRenderer.cs
@@ -1,5 +1,9 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Numerics;
+using Vector2 = Godot.Vector2;
 
 public partial class ComplexRenderer : ViewBase
 {
@@ -8,6 +12,9 @@ public partial class ComplexRenderer : ViewBase
     [Export] public Button magnitudeBox;
 
     [Export] public Button derivativeBox;
+
+    [Export] public RecompileComplexRenderer compiler;
+    [Export] public Plotter plotter;
 
 
     public bool magnitude;
@@ -29,6 +36,25 @@ public partial class ComplexRenderer : ViewBase
         {
             derivative = !derivative;
             derivativeBox.SetPressedNoSignal(derivative);
+        }
+        if (Input.IsActionPressed("Click"))
+        {
+            List<Vector2> points = new List<Vector2>();
+            Vector2 mouse = GetViewport().GetMousePosition() + new Vector2(-_w / 2, -_h / 2);
+            Vector2 scale = (mouse / _w / zoom) + offset;
+            Complex start = new Complex(scale.X, scale.Y);
+            List<Vector2> vector2List = new List<Vector2>();
+
+            Complex newNumber = compiler.function(start, 0);
+            Complex pointPixel = ((newNumber - new Complex(offset.X, offset.Y)) * zoom * _w);
+            Complex startPoint = ((start - new Complex(offset.X, offset.Y)) * zoom * _w);
+            vector2List.Add(new Vector2((float)startPoint.Real, (float)startPoint.Imaginary));
+            vector2List.Add(new Vector2((float)pointPixel.Real, (float)pointPixel.Imaginary));
+            plotter.SetPoints(vector2List);
+        }
+        else
+        {
+            plotter.SetPoints(new List<Vector2>());
         }
     }
 
